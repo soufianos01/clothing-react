9edecfc06599b9b54c4a3b5337f3f7a7c8b3d2bb
Souphiane Bensalim <souphianebensalim@gmail.com>
2020-07-28 09:20:14 +0200

    styling menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <HomePage />
     </div>
   );
 }
