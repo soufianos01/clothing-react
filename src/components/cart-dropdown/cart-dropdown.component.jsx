d8ccb0c852315663ea540cfd9a4b1285bb6a2259
Souphiane Bensalim <souphianebensalim@gmail.com>
2020-08-14 12:34:42 +0200

    adding memorisation with reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ items }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {
+        items.map(item => (
+          <CartItem key={item.id} item={item}/>
+        ))
+      }
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+  items: selectCartItems(state)
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
       {
         items
           .filter((itm,idx) => idx < 4)
-          .map(({id, ...otherItemProps}) => (
-            <CollectionItem key={id} {...otherItemProps} />
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
           ))
       }
     </div>
